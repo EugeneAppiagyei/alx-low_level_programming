6cc07724b9f522343dcc892222b70ca8b9477dfb
EugeneFreda <eugeneappiagyei986@gmail.com>
2022-07-02 09:48:52 -0700

    added num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - entry point
+ *
+ *Return: always 0
+ */
+int main(void)
+
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
