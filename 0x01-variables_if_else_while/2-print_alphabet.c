609196857065fcb275c3844a667fabda5b56bf03
EugeneFreda <eugeneappiagyei986@gmail.com>
2022-07-02 07:04:52 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return - always 0
+ */
+int main(void)
+
+{
+	int ch;
+
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+	return (0);
+}
