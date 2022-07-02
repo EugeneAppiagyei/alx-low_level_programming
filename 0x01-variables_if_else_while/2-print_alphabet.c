4430f63b492359c48fb72f171ce2189d7c5c57f6
EugeneFreda <eugeneappiagyei986@gmail.com>
2022-07-02 07:29:59 -0700

    alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
- * main - entry point
+ * main -  assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
  *
  * Return - always 0
  */
@@ -15,6 +16,6 @@ int main(void)
 	{
 		putchar(ch);
 	}
-	putchar(10);
+	putchar(10); /* this is an ascii code for new line*/
 	return (0);
 }
