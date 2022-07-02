d693a2ff57bb4a22d2cd54bd1c42d5fe01c1699c
EugeneFreda <eugeneappiagyei986@gmail.com>
2022-07-02 10:44:09 -0700

    add 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+
+	putchar(10);
+
+	return (0);
+
+}
